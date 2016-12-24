2cdf55dea94821e57086f441fa91904f3550da73
Yuto Takei <yuto.takei@bitflyer.com>
2016-12-24 15:49:23 +0900

    Initial commit

---
+++ MinChain/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MinChain
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
